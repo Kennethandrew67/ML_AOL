ccb6908649dd97abb9f8aca3703e29bdfd2d0f2d
KennethAndrew <kennthandrew18@gmail.com>
2025-07-02 10:19:48 +0700

    Ft. Fix

--- app.py
+++ app.py
@@ -4,22 +4,13 @@ import pickle
 import tensorflow as tf
 from PIL import Image
 from tensorflow.keras.models import Model
-from tensorflow.keras.applications.inception_v3 import InceptionV3, preprocess_input
+from tensorflow.keras.applications.mobilenet_v2 import MobileNetV2, preprocess_input
 from tensorflow.keras.preprocessing.image import img_to_array
 from tensorflow.keras.preprocessing.sequence import pad_sequences
-from tensorflow.keras.layers import GlobalAveragePooling2D
-from tensorflow.keras import Input
 
 
-# Load InceptionV3 base model without top
-base_model = InceptionV3(weights='imagenet', include_top=False, input_shape=(299, 299, 3))
-
-# Add GlobalAveragePooling2D layer on top
-x = GlobalAveragePooling2D()(base_model.output)
-inception_model = Model(inputs=base_model.input, outputs=x)
-
-cnn_output_dim = 2048
-
+mobilenet_model = MobileNetV2(weights="imagenet")
+mobilenet_model = Model(inputs=mobilenet_model.inputs, outputs=mobilenet_model.layers[-2].output)
 
 model = tf.keras.models.load_model('caption_model.h5', compile=False)
 
@@ -43,21 +34,20 @@ uploaded_image = st.file_uploader("Choose an image", type=["jpg", "jpeg", "png"]
 def get_word_from_index(index, tokenizer):
     return next((word for word, idx in tokenizer.word_index.items() if idx == index), None)
 
-def greedy_generator(image_features):
-    in_text = 'start '
+def predict_caption(model, image_features, tokenizer, max_caption_length):
+    caption = "startseq"
     for _ in range(max_caption_length):
-        sequence = tokenizer.texts_to_sequences([in_text])[0]
-        sequence = pad_sequences([sequence], maxlen=max_caption_length).reshape((1, max_caption_length))
-        prediction = model.predict([image_features.reshape(1, cnn_output_dim), sequence], verbose=0)
-        idx = np.argmax(prediction)
-        word = tokenizer.index_word.get(idx)
-        if word is None:
+        sequence = tokenizer.texts_to_sequences([caption])[0]
+        sequence = pad_sequences([sequence], maxlen=max_caption_length)
+        yhat = model.predict([image_features, sequence], verbose=0)
+        predicted_index = np.argmax(yhat)
+        predicted_word = get_word_from_index(predicted_index, tokenizer)
+        if predicted_word is None:
             break
-        in_text += ' ' + word
-        if word == 'end':
+        caption += " " + predicted_word
+        if predicted_word == "endseq":
             break
-    return in_text.replace('start ', '').replace(' end', '')
-
+    return caption.replace("startseq", "").replace("endseq", "").strip()
 
 # Process uploaded image
 if uploaded_image is not None:
@@ -66,15 +56,16 @@ if uploaded_image is not None:
 
     st.subheader("Generated Caption")
     with st.spinner("Generating caption..."):
-        # Preprocess uploaded image
-        image = Image.open(uploaded_image).resize((299, 299)).convert('RGB')
+        # Load and preprocess image using PIL
+        image = Image.open(uploaded_image).resize((224, 224)).convert('RGB')
         image = img_to_array(image)
         image = np.expand_dims(image, axis=0)
         image = preprocess_input(image)
 
-        # Extract image features
-        image_features = inception_model.predict(image, verbose=0)
+        # Extract features using MobileNetV2
+        image_features = mobilenet_model.predict(image, verbose=0)
 
         # Generate caption
-        generated_caption = greedy_generator(image_features)
+        generated_caption = predict_caption(model, image_features, tokenizer, max_caption_length)
+
         st.markdown(f"**{generated_caption}**")
