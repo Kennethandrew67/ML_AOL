98aea9f0ad8db07e7b7aafc6ab57a4bf8112b234
KennethAndrew <kennthandrew18@gmail.com>
2025-07-02 10:05:17 +0700

    Update app.py

--- app.py
+++ app.py
@@ -4,13 +4,15 @@ import pickle
 import tensorflow as tf
 from PIL import Image
 from tensorflow.keras.models import Model
-from tensorflow.keras.applications.mobilenet_v2 import MobileNetV2, preprocess_input
+from tensorflow.keras.applications.inception_v3 import InceptionV3, preprocess_input
 from tensorflow.keras.preprocessing.image import img_to_array
 from tensorflow.keras.preprocessing.sequence import pad_sequences
 
 
-mobilenet_model = MobileNetV2(weights="imagenet")
-mobilenet_model = Model(inputs=mobilenet_model.inputs, outputs=mobilenet_model.layers[-2].output)
+# Load InceptionV3 model pre-trained on ImageNet, exclude top classification layer
+inception_model = InceptionV3(weights='imagenet')
+inception_model = Model(inputs=inception_model.input, outputs=inception_model.get_layer('avg_pool').output)
+
 
 model = tf.keras.models.load_model('caption_model.h5', compile=False)
 
@@ -57,13 +59,14 @@ if uploaded_image is not None:
     st.subheader("Generated Caption")
     with st.spinner("Generating caption..."):
         # Load and preprocess image using PIL
-        image = Image.open(uploaded_image).resize((224, 224)).convert('RGB')
+        image = Image.open(uploaded_image).resize((299, 299)).convert('RGB')
         image = img_to_array(image)
         image = np.expand_dims(image, axis=0)
         image = preprocess_input(image)
 
-        # Extract features using MobileNetV2
-        image_features = mobilenet_model.predict(image, verbose=0)
+        # Extract features using InceptionV3
+        image_features = inception_model.predict(image, verbose=0)
+
 
         # Generate caption
         generated_caption = predict_caption(model, image_features, tokenizer, max_caption_length)
